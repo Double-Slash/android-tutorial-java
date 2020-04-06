c1d1e1e2f9beca55b9f39bc2d19fbfe28be510ad
JuHyang <kkss2889@naver.com>
2020-04-06 13:27:21 +0900

    1st Practice

--- app/src/main/java/com/slash/slash/android_tutorial_java/MainActivity.java
+++ app/src/main/java/com/slash/slash/android_tutorial_java/MainActivity.java
@@ -1,31 +1,35 @@
 package com.slash.slash.android_tutorial_java;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    HelloWorld helloWorld = new HelloWorld("이주향", 25, 4);
+    TextView textViewMain;
+    EditText editTextMain;
+    Button buttonMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        helloWorld.name = null;
+        textViewMain = findViewById(R.id.textView_main); // findViewById 를 통해서 xml 의 id 와 각 View 의 객체를 연결시켜준다
+        editTextMain = findViewById(R.id.editText_main);
+        buttonMain = findViewById(R.id.button_main);
 
-        ArrayList<HelloWorld> helloWorlds = new ArrayList<>();
-
-        for (HelloWorld hello : helloWorlds) {
-            System.out.println(hello.name);
-        }
-
-        for (int i = 0; i < 11; i++) {
-            System.out.println(i);
-        }
+        buttonMain.setOnClickListener(new View.OnClickListener() { // buttonMain 에 이벤트 리스너를 만들어서 넣어준다
+            @Override
+            public void onClick(View view) {
+                String input = editTextMain.getText().toString(); // editText 로부터 Text 를 가져오는 방법 !
+                textViewMain.setText(input); // TextView 에 문자열을 넣는 방법 !
+            }
+        });
     }
 }
 
